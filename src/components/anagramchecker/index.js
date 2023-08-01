cda2dbd81e10af1181b5583e45d6e80c3c019671
Jeno Rigor <jeno.rigor@proposify.com>
2023-08-01 16:38:21 -0700

    chore: created hook useAreStringsAnagrams
    Moved logic for checking if strings are anagrams to hook.

--- src/components/anagramchecker/index.js
+++ src/components/anagramchecker/index.js
@@ -1,44 +1,29 @@
 import StringInput from "../stringinput";
-import {useCallback, useEffect, useState} from "react";
-
-const areStringsAnagrams = (firstString, secondString) => {
-    if (
-        firstString.length !== secondString.length ||
-        firstString === secondString
-    )
-    {
-        return false;
-    }
-
-    const sortFn = (a,b) => a.toLowerCase().localeCompare(b.toLowerCase());
-    let s1 = Array.from(firstString).sort((a,b) => sortFn(a,b));
-    let s2 = Array.from(secondString).sort((a,b) => sortFn(a,b));
-
-    return s1.join('') === s2.join('');
-}
+import { useCallback, useEffect, useState } from "react";
+import { useAreStringsAnagrams } from "../../hooks/useAreStringsAnagrams";
 
 const AnagramChecker = () => {
     const [firstString, setFirstString] = useState('');
     const [secondString, setSecondString] = useState('');
     const [areAnagrams, setAreAnagrams] = useState(false);
+    const { areStringsAnagrams } = useAreStringsAnagrams();
 
     const compareStrings = useCallback(() => {
         setAreAnagrams(areStringsAnagrams(firstString, secondString))
-    }, [firstString, secondString]);
+    }, [firstString, secondString, areStringsAnagrams]);
 
     useEffect(() => {
         compareStrings();
     },[firstString, secondString, compareStrings]);
 
-
     return (
         <div className="w-full max-w-lg">
-            <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-23l lg:text-3xl dark:text-white">
+            <h1 className="mb-4">
                 Simple Anagram Checker
             </h1>
             {areAnagrams ?
                 <div
-                    className="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                    className="p-4 mb-4 text-sm"
                     role="alert">
                     <span className="font-medium">Tada!</span> {firstString} and {secondString} are ANAGRAMS!
                 </div> : null
@@ -48,7 +33,7 @@ const AnagramChecker = () => {
                     <StringInput id={1} value={firstString} setValue={setFirstString} />
                 </div>
                 <div className="mb-2">
-                    <StringInput id={2} value={secondString} setValue={setSecondString}/>
+                    <StringInput id={2} value={secondString} setValue={setSecondString} />
                 </div>
             </form>
         </div>
---
+++ src/hooks/useAreStringsAnagrams.js
@@ -0,0 +1,21 @@
+export const useAreStringsAnagrams = () => {
+    const areStringsAnagrams = (firstString, secondString) => {
+        if (
+            firstString.length !== secondString.length ||
+            firstString === secondString
+        )
+        {
+            return false;
+        }
+
+        const sortFn = (a,b) => a.toLowerCase().localeCompare(b.toLowerCase());
+        let s1 = Array.from(firstString).sort((a,b) => sortFn(a,b));
+        let s2 = Array.from(secondString).sort((a,b) => sortFn(a,b));
+
+        return s1.join('') === s2.join('');
+    }
+
+    return {
+        areStringsAnagrams
+    };
+}
